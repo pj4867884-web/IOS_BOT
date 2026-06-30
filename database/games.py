c93978e83bbff0195c1658305a7b4c32e5cc13c3
pj4867884-web <pj4867884@gmail.com>
2026-07-01 00:48:19 +0530

    Create games.py

---
+++ database/games.py
@@ -0,0 +1,49 @@
+GAMES = {
+    "KING iOS": {
+        "1 Day": 200,
+        "2 Weeks": 799,
+        "1 Month": 2000,
+    },
+
+    "WINIOS BGMI": {
+        "1 Day": 180,
+        "1 Week": 600,
+        "1 Month": 1299,
+    },
+
+    "IOS ZOON": {
+        "1 Day": 200,
+        "1 Week": 500,
+        "1 Month": 1499,
+    },
+
+    "DOLPHIN IOS": {
+        "1 Day": 200,
+        "1 Week": 800,
+        "1 Month": 1700,
+    },
+
+    "NEXT IOS": {
+        "1 Day": 200,
+        "1 Week": 799,
+        "1 Month": 2000,
+    },
+
+    "VNHAX PRO": {
+        "1 Day": 200,
+        "1 Week": 700,
+        "1 Month": 1599,
+    },
+
+    "MARS LOADER": {
+        "1 Day": 130,
+        "1 Week": 500,
+        "1 Month": 999,
+    },
+
+    "DEADEYE": {
+        "1 Day": 150,
+        "1 Week": 600,
+        "1 Month": 1499,
+    }
+}
