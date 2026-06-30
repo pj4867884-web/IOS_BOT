f973a8914c307120bb4c2b03814e307ee7230c91
pj4867884-web <pj4867884@gmail.com>
2026-07-01 00:52:57 +0530

    Update payment.py

--- handlers/payment.py
+++ handlers/payment.py
@@ -1,7 +1,34 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from config import ADMIN_ID
+
+
 async def photo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    user = update.message.from_user
+
+    caption = f"""
+💳 New Payment Received
+
+👤 Name : {user.first_name}
+🆔 User ID : {user.id}
+📛 Username : @{user.username if user.username else 'None'}
+
+Please Verify Payment.
+"""
+
+    await context.bot.forward_message(
+        chat_id=ADMIN_ID,
+        from_chat_id=update.effective_chat.id,
+        message_id=update.message.message_id,
+    )
+
+    await context.bot.send_message(
+        chat_id=ADMIN_ID,
+        text=caption
+    )
+
     await update.message.reply_text(
-        "✅ Payment screenshot received.\n\nYour payment is under review.\nPlease wait for admin approval."
+        "✅ Payment screenshot received.\n\nPlease wait for admin approval."
     )
